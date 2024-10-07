128e964ff44860f6b9861afb399cef51a894d7db
devop <devopsshakh@gmail.com>
2024-10-07 18:40:56 +0500

    view qoshildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_test/view/home_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('Git work'),
-        backgroundColor: Colors.blue,),
-      ),
+      home: HomePage(),
     );
   }
 }
